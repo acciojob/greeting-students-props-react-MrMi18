fc763a753b5bae46aa43f03c73b03d283eff85e8
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-07-10 10:33:40 +0530

    Added p tag

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,9 @@ import React from 'react'
 function Welcome(props) {
   return (
     <div>
-        <h1>Hey {props.name}!</h1>
-        <h2>Welcome to school</h2>
+      <h1>Hey! {props.name}</h1>
+      <h2>Welcome to School.</h2>
+      <p>{props.name}</p>
     </div>
   )
 }
